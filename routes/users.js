fb93249db21a8136c843fc103578a824413a3552
Christina Vekri <christina.vek@gmail.com>
2024-07-11 14:43:59 +0200

    added the middleware

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import {getUser, registerUser, deleteUser } from '../controllers/usersControllers.js';
+import {getUser, registerUser, deleteUser, loginUser } from '../controllers/usersControllers.js';
+import { authMiddleware } from '../middleware/userAuth.js';
 
 const usersRouter = express.Router();
 
-usersRouter.get('/', getUser);
+usersRouter.get('/',authMiddleware, getUser);
+usersRouter.post('/login',loginUser);
 usersRouter.post('/register', registerUser);
 usersRouter.delete('/:id', deleteUser);
 
